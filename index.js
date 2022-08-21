053bb28e5de2686fd734f0bd6139d0c1d03fc1f1
Aikokul <baatirbekovna.6@mail.ru>
2022-08-21 16:48:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let numEl = document.getElementById("one");
+let numEl2 = document.getElementById("two");
+let num = 0;
+
+function add1() {
+  num += 1;
+  numEl.textContent = num;
+}
+function add2() {
+  num += 2;
+  numEl.textContent = num;
+}
+function add3() {
+  num += 3;
+  numEl.textContent = num;
+}
+function add11() {
+  num += 1;
+  numEl2.textContent = num;
+}
+function add12() {
+  num += 2;
+  numEl2.textContent = num;
+}
+function add13() {
+  num += 3;
+  numEl2.textContent = num;
+}
